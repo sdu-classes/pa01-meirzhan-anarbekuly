54195b3ee4a9b7e40ba448328bfcc2383e9078bf
meirzhan <124430073+meirzhan-anarbekuly@users.noreply.github.com>
2023-02-04 14:59:15 +0600

    Update TaskOne.java

--- TaskOne.java
+++ TaskOne.java
@@ -1,1 +1,44 @@
+public class Circle {
+    double radius = 1.0;
+
+    public Circle(){
+    }
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius=radius;
+    }
+
+    public double getArea(){
+        radius= Math.PI*radius*radius;
+        return radius;
+    }
+
+    public double getCircumference(){
+        radius=2*Math.PI*radius;
+        return radius;
+    }
+
+    public String toString(){
+        return "Circle[radius="+radius+"]";
+    }
+
+    public static void main(String[] args) {
+        Circle c1=new Circle();
+
+
+        c1.getArea();
+        System.out.println(c1);
+        c1.getCircumference();
+        System.out.println(c1);
+    }
+
+}
 
